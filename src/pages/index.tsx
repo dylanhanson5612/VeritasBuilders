6aa39520c62464722a0099f8bee0bd2b129de10b
dylanhanson5612 <dylan.hanson123@gmail.com>
2026-02-20 10:06:57 -0500

    Create a basic Next.js home page component

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to Veritas Builders</h1>
+      <p>Your one-stop solution for building amazing applications!</p>
+    </div>
+  );
+};
+
+export default Home;
